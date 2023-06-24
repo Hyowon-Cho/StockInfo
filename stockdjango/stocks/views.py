319c4448c49d81d2105398fa86d080027b57343d
Hyowon-Cho <baltimorecho@gmail.com>
2023-06-24 13:24:27 +0900

    StockInfo Django Project

--- stockdjango/stocks/views.py
+++ stockdjango/stocks/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
 # Create your views here.
+import requests
+import json
+
+def home(request):
+
+    try:
+        ticker = request.GET['ticker']
+        stock_api = requests.get(
+            "https://cloud.iexapis.com/stable/stock/" + ticker + "/quote?token=pk_1beff6d0b97b4cfcac54956d551dd530")
+        stock = json.loads(stock_api.content)
+    except Exception as e:
+        stock = ""
+
+    content = {'stock':stock}
+
+    return render(request, 'stocks/home.html', content)
